fd2ff5c556c0823ec4d7d91d31146f812f8d508a
Derrick Hammer <derrick@derrickhammer.com>
2023-09-02 07:02:53 -0400

    feat: add support for creating a resolver CID for the module if MODULE_SEED is not false

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as process from "process";
 import fs from "fs/promises";
 import path from "path";
 import {
+  concatBytes,
   hexToBytes,
   maybeInitDefaultPortals,
   setActivePortalMasterKey,
@@ -12,7 +13,28 @@ import {
 } from "@lumeweb/libweb";
 import chalk from "chalk";
 import * as util from "util";
-import { fileExists } from "#utils.js";
+import { fileExists } from "./utils.js";
+import * as bip39 from "@scure/bip39";
+import { wordlist } from "@scure/bip39/wordlists/english";
+import { HDKey } from "ed25519-keygen/hdkey";
+
+import {
+  CID_HASH_TYPES,
+  CID_TYPES,
+  createKeyPair,
+  createNode,
+  Logger,
+  REGISTRY_TYPES,
+  S5NodeConfig,
+} from "@lumeweb/libs5";
+
+import { MemoryLevel } from "memory-level";
+import { base58btc } from "multiformats/bases/base58";
+import KeyPairEd25519 from "@lumeweb/libs5/lib/ed25519.js";
+import defer from "p-defer";
+import { encodeCid } from "@lumeweb/libportal";
+
+const BIP44_PATH = "m/44'/1627'/0'/0'/0'";
 
 let key = process.env.PORTAL_PRIVATE_KEY;
 
@@ -20,10 +42,26 @@ if (!key) {
   // @ts-ignore
   key = await prompts.prompts.password({
     name: "private_key",
-    message: "Enter your private key",
+    message: "Enter your portal private key",
     validate: (prev) => prev && prev.length === 64,
   });
 }
+
+let seed = process.env.MODULE_SEED;
+if (!seed && seed === undefined) {
+  // @ts-ignore
+  seed = await prompts.prompts.password({
+    name: "module_seed",
+    message: "Enter your module seed",
+    validate: (prev) => prev && bip39.validateMnemonic(prev, wordlist),
+  });
+}
+
+const hdKey = seed
+  ? HDKey.fromMasterSeed(await bip39.mnemonicToSeed(seed as string)).derive(
+      BIP44_PATH,
+    )
+  : false;
 
 let file = process.env.MODULE_FILE;
 
@@ -82,3 +120,79 @@ console.log(
     cid,
   ),
 );
+
+if (!hdKey) {
+  process.exit(0);
+}
+
+const db = new MemoryLevel<string, Uint8Array>({
+  storeEncoding: "view",
+  valueEncoding: "buffer",
+});
+await db.open();
+
+let config = {
+  keyPair: createKeyPair(),
+  db,
+  p2p: {
+    peers: {
+      initial: [
+        //  ...BOOTSTRAP_NODES,
+        "ws://z2DX2geLzZnff444mUFXZj7F3J2PA4SbRbfUiPwofk8Umix@127.0.0.1:5050/s5/p2p",
+      ],
+    },
+  },
+  logger: {
+    info: (s: string) => {},
+    verbose: (s: string) => {},
+    warn: (s: string) => {},
+    error: (s: string) => {},
+    catched: (e: any, context?: string | null) => {},
+  },
+} as S5NodeConfig;
+
+const node = createNode(config);
+await node.start();
+
+const peerDefer = defer();
+
+node.services.p2p.once("peerConnected", peerDefer.resolve);
+
+await peerDefer.promise;
+{
+  const cidBytes = base58btc.decode(cid);
+  const key = hdKey as HDKey;
+
+  let revision = 0;
+
+  const ret = await node.services.registry.get(
+    new KeyPairEd25519(key.privateKey).publicKey,
+  );
+
+  if (ret) {
+    revision = ret.revision + 1;
+  }
+  const sre = node.services.registry.signRegistryEntry({
+    kp: new KeyPairEd25519((hdKey as HDKey).privateKey),
+    data: concatBytes(
+      Uint8Array.from([
+        REGISTRY_TYPES.CID,
+        CID_TYPES.RESOLVER,
+        CID_HASH_TYPES.BLAKE3,
+      ]),
+      cidBytes,
+    ),
+    revision,
+  });
+
+  await node.services.registry.set(sre);
+
+  console.log(
+    util.format(
+      "%s: %s",
+      chalk.green("Resolver entry"),
+      encodeCid(cidBytes, 0, CID_TYPES.RESOLVER, CID_HASH_TYPES.ED25519),
+    ),
+  );
+  await node.stop();
+}
